b4301d52a0f1fd3d374e5b7258252efc459cf10a
basvantol <33867754+basvantol@users.noreply.github.com>
2018-01-10 10:53:38 +0100

    icon update

---
+++ SPARK/Spatial_decision_making_Freek_Bas/help/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Spatial_decision_making_Freek_Bas\u2019s documentation!"],titleterms:{document:0,indic:0,spatial_decision_making_freek_ba:0,tabl:0,welcom:0}})

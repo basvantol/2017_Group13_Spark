0f219227e142a9dab548d65219f5321b3c73c6b7
basvantol <33867754+basvantol@users.noreply.github.com>
2018-01-12 13:52:30 +0100

    trying to route

--- SPARK/Spatial_decision_making_Freek_Bas/spark_dockwidget.py
+++ SPARK/Spatial_decision_making_Freek_Bas/spark_dockwidget.py
@@ -70,14 +70,16 @@ class Spatial_decision_making_Freek_BasDockWidget(QtGui.QDockWidget, FORM_CLASS)
 
         #input
         self.ConfirmButtonAccount.clicked.connect(self.ConfirmAccount)
-        self.ConfirmButtonDestination.clicked.connect(self.ConfirmDestination)
+        self.RateSpot.clicked.connect(self.ConfirmDestination)
+        self.RateSpot.clicked.connect(self.deleteRoutes)
         self.ConfirmButtonRating.clicked.connect(self.ConfirmRating)
         self.EditButtonAccount.clicked.connect(self.EditAccount)
+        self.ConfirmButtonDestination.clicked.connect(self.buildNetwork)
         self.ShowRoute.clicked.connect(self.calculateRoute)
 
-    self.logoLabel.setPixmap(QtGui.QPixmap(self.plugin_dir + '/icons/Spark.png'))
+        self.logoLabel.setPixmap(QtGui.QPixmap(self.plugin_dir + '/icons/Spark.png'))
 
-        elf.graph = QgsGraph()
+        self.graph = QgsGraph()
         self.tied_points = []
 
     def closeEvent(self, event):
@@ -118,55 +120,75 @@ class Spatial_decision_making_Freek_BasDockWidget(QtGui.QDockWidget, FORM_CLASS)
         self.tabWidget.setCurrentIndex(0)
 
 
-    def buildNetwork(self):
-        self.network_layer = self.getNetwork()
-        if self.network_layer:
-            # get the points to be used as origin and destination
-            # in this case gets the centroid of the selected features
-            selected_sources = self.getSelectedLayer().selectedFeatures()
-            source_points = [feature.geometry().centroid().asPoint() for feature in selected_sources]
-            # build the graph including these points
-            if len(source_points) > 1:
-                self.graph, self.tied_points = uf.makeUndirectedGraph(self.network_layer, source_points)
-                # the tied points are the new source_points on the graph
-                if self.graph and self.tied_points:
-                    text = "network is built for %s points" % len(self.tied_points)
-                    self.insertReport(text)
-        return
-
-    def calculateRoute(self):
-        # origin and destination must be in the set of tied_points
-        options = len(self.tied_points)
-        if options > 1:
-            # origin and destination are given as an index in the tied_points list
-            origin = 0
-            destination = random.randint(1,options-1)
-            # calculate the shortest path for the given origin and destination
-            path = uf.calculateRouteDijkstra(self.graph, self.tied_points, origin, destination)
-            # store the route results in temporary layer called "Routes"
-            routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
-            # create one if it doesn't exist
-            if not routes_layer:
-                attribs = ['id']
-                types = [QtCore.QVariant.String]
-                routes_layer = uf.createTempLayer('Routes','LINESTRING',self.network_layer.crs().postgisSrid(), attribs, types)
-                uf.loadTempLayer(routes_layer)
-            # insert route line
-            for route in routes_layer.getFeatures():
-                print route.id()
-            uf.insertTempFeatures(routes_layer, [path], [['testing',100.00]])
-            buffer = processing.runandload('qgis:fixeddistancebuffer',routes_layer,10.0,5,False,None)
-            #self.refreshCanvas(routes_layer)
-
-    def deleteRoutes(self):
-        routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
-        if routes_layer:
-            ids = uf.getAllFeatureIds(routes_layer)
-            routes_layer.startEditing()
-            for id in ids:
-                routes_layer.deleteFeature(id)
-            routes_layer.commitChanges()
-
-
+    # def buildNetwork(self):
+    #     self.network_layer = self.getNetwork()
+    #     if self.network_layer:
+    #         # get the points to be used as origin and destination
+    #         # in this case gets the centroid of the selected features
+    #         selected_sources = self.getSelectedLayer().selectedFeatures()
+    #         source_points = [feature.geometry().centroid().asPoint() for feature in selected_sources]
+    #         # build the graph including these points
+    #         if len(source_points) > 1:
+    #             self.graph, self.tied_points = uf.makeUndirectedGraph(self.network_layer, source_points)
+    #             # the tied points are the new source_points on the graph
+    #             if self.graph and self.tied_points:
+    #                 text = "network is built for %s points" % len(self.tied_points)
+    #                 self.insertReport(text)
+    #     return
+    #
+    # def calculateRoute(self):
+    #     # origin and destination must be in the set of tied_points
+    #     options = len(self.tied_points)
+    #     if options > 1:
+    #         # origin and destination are given as an index in the tied_points list
+    #         origin = 0
+    #         destination = random.randint(1,options-1)
+    #         # calculate the shortest path for the given origin and destination
+    #         path = uf.calculateRouteDijkstra(self.graph, self.tied_points, origin, destination)
+    #         # store the route results in temporary layer called "Routes"
+    #         routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
+    #         # create one if it doesn't exist
+    #         if not routes_layer:
+    #             attribs = ['id']
+    #             types = [QtCore.QVariant.String]
+    #             routes_layer = uf.createTempLayer('Routes','LINESTRING',self.network_layer.crs().postgisSrid(), attribs, types)
+    #             uf.loadTempLayer(routes_layer)
+    #         # insert route line
+    #         for route in routes_layer.getFeatures():
+    #             print route.id()
+    #         uf.insertTempFeatures(routes_layer, [path], [['testing',100.00]])
+    #         buffer = processing.runandload('qgis:fixeddistancebuffer',routes_layer,10.0,5,False,None)
+    #         #self.refreshCanvas(routes_layer)
+    #
+    # def deleteRoutes(self):
+    #     routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
+    #     if routes_layer:
+    #         ids = uf.getAllFeatureIds(routes_layer)
+    #         routes_layer.startEditing()
+    #         for id in ids:
+    #             routes_layer.deleteFeature(id)
+    #         routes_layer.commitChanges()
+    #
+    # def getNetwork(self):
+    #     roads_layer = self.getSelectedLayer()
+    #     if roads_layer:
+    #         # see if there is an obstacles layer to subtract roads from the network
+    #         obstacles_layer = uf.getLegendLayerByName(self.iface, "Obstacles")
+    #         if obstacles_layer:
+    #             # retrieve roads outside obstacles (inside = False)
+    #             features = uf.getFeaturesByIntersection(roads_layer, obstacles_layer, False)
+    #             # add these roads to a new temporary layer
+    #             road_network = uf.createTempLayer('Temp_Network','LINESTRING',roads_layer.crs().postgisSrid(),[],[])
+    #             road_network.dataProvider().addFeatures(features)
+    #         else:
+    #             road_network = roads_layer
+    #         return road_network
+    #     else:
+    #         return
+    #
+    # def getSelectedLayer(self):
+    #     layer_name = self.selectLayerCombo.currentText()
+    #     layer = uf.getLegendLayerByName(self.iface,layer_name)
+    #     return layer
 
 

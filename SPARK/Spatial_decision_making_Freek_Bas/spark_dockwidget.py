7c353c47a5c579138247449d0a47026303ff73d5
basvantol <33867754+basvantol@users.noreply.github.com>
2018-01-13 00:59:05 +0100

    dictionary working

--- SPARK/Spatial_decision_making_Freek_Bas/spark_dockwidget.py
+++ SPARK/Spatial_decision_making_Freek_Bas/spark_dockwidget.py
@@ -66,6 +66,9 @@ class Spatial_decision_making_Freek_BasDockWidget(QtGui.QDockWidget, FORM_CLASS)
         self.plugin_dir = os.path.dirname(__file__)
         self.openScenario()
 
+        self.layers_dic = self.getLayers()
+
+
 
         #input
         self.ConfirmButtonAccount.clicked.connect(self.ConfirmAccount)
@@ -102,6 +105,7 @@ class Spatial_decision_making_Freek_BasDockWidget(QtGui.QDockWidget, FORM_CLASS)
         layers_dict['roads'] = layer_names[3]
         layers_dict['rating'] = layer_names[4]
         layers_dict['account'] = layer_names[5]
+        return layers_dict
 
     def ConfirmAccount(self):
         self.LogList.append(self.HomeAddressInput.text())
@@ -161,62 +165,4 @@ class Spatial_decision_making_Freek_BasDockWidget(QtGui.QDockWidget, FORM_CLASS)
         self.TabRating.setEnabled(False)
         self.EditButtonAccount.setEnabled(False)
         self.tabWidget.setCurrentIndex(0)
-
-    #
-    # def buildNetwork(self):
-    #     self.network_layer = self.getNetwork() network_layer
-    #     if self.network_layer:
-    #         # get the points to be used as origin and destination
-    #         # in this case gets the centroid of the selected features
-    #         selected_sources = self.getSelectedLayer().selectedFeatures()
-    #         source_points = [feature.geometry().centroid().asPoint() for feature in selected_sources]
-    #         # build the graph including these points
-    #         if len(source_points) > 1:
-    #             self.graph, self.tied_points = uf.makeUndirectedGraph(self.network_layer, source_points)
-    #             # the tied points are the new source_points on the graph
-    #             if self.graph and self.tied_points:
-    #                 text = "network is built for %s points" % len(self.tied_points)
-    #                 self.insertReport(text)
-    #     return
-    #
-    # def calculateRoute(self):
-    #     # origin and destination must be in the set of tied_points
-    #     options = len(self.tied_points)
-    #     if options > 1:
-    #         # origin and destination are given as an index in the tied_points list
-    #         origin = 0
-    #         destination = random.randint(1,options-1)
-    #         # calculate the shortest path for the given origin and destination
-    #         path = uf.calculateRouteDijkstra(self.graph, self.tied_points, origin, destination)
-    #         # store the route results in temporary layer called "Routes"
-    #         routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
-    #         # create one if it doesn't exist
-    #         if not routes_layer:
-    #             attribs = ['id']
-    #             types = [QtCore.QVariant.String]
-    #             routes_layer = uf.createTempLayer('Routes','LINESTRING',self.network_layer.crs().postgisSrid(), attribs, types)
-    #             uf.loadTempLayer(routes_layer)
-    #         # insert route line
-    #         for route in routes_layer.getFeatures():
-    #             print route.id()
-    #         uf.insertTempFeatures(routes_layer, [path], [['testing',100.00]])
-    #         buffer = processing.runandload('qgis:fixeddistancebuffer',routes_layer,10.0,5,False,None)
-    #         #self.refreshCanvas(routes_layer)
-    #
-    # def deleteRoutes(self):
-    #     routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
-    #     if routes_layer:
-    #         ids = uf.getAllFeatureIds(routes_layer)
-    #         routes_layer.startEditing()
-    #         for id in ids:
-    #             routes_layer.deleteFeature(id)
-    #         routes_layer.commitChanges()
-    #
-
-    #
-    # def getSelectedLayer(self):
-    #     layer_name = self.selectLayerCombo.currentText()
-    #     layer = uf.getLegendLayerByName(self.iface,layer_name)
-    #     return layer
-
 
